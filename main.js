8880b3cf9fccdabb9a4d62763f7824660f7db6bb
giovany osorio <giovany.torres.799@gmail.com>
2023-09-18 21:55:40 -0500

    new endpoint

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-console.log("hello world")
-
 const URL="https://restcountries.com/v3.1/all"
 
+const capital="https://restcountries.com/v3.1/capital/berlin"
+
+const spanerror=document.getElementById("error")
 
 async function fetchData(){
     const response=await fetch(URL)
@@ -40,3 +41,29 @@ myButton.addEventListener("click", () => {
     return fetchData(URL);
   });
 
+  async function favorites(){
+    const response=await fetch(capital)
+    const data=await response.json()
+    console.log(data,"data capital")
+/*     const image1=document.getElementById("image1")
+    const image2=document.getElementById("image2")
+    const image3=document.getElementById("image3")
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]]; // Intercambia elementos
+        }
+        return array;
+    }
+    const randomFlags = shuffleArray([...data]).slice(0, 3);
+
+    console.log(randomFlags);
+
+
+    image1.src=randomFlags[0].flags.png
+    image2.src=randomFlags[1].flags.png
+    image3.src=randomFlags[2].flags.png */
+   // name.innerHTML=pais.altSpellings[1]
+  //  capital.innerHTML=pais.capital[0]
+}
+favorites()
